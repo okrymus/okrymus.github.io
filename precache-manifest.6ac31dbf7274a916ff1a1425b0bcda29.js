dd9fe86388bf85e2ed5f6756d14e9a697a6d0650
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-01-21 21:21:11 -0500

    Updates

---
+++ precache-manifest.6ac31dbf7274a916ff1a1425b0bcda29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "639aeeb03578e370b4fd",
+    "url": "/static/js/main.639aeeb0.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "639aeeb03578e370b4fd",
+    "url": "/static/css/main.0e51b3b4.chunk.css"
+  },
+  {
+    "revision": "79fd84510e38a3f467e551f45a99b294",
+    "url": "/index.html"
+  }
+];
