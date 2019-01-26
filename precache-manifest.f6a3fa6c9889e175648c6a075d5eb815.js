330d821a95e188617611fea2bccd9582239183a1
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-01-26 14:21:30 -0500

    Updates

---
+++ precache-manifest.f6a3fa6c9889e175648c6a075d5eb815.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "554c132f3a468ea748aa",
+    "url": "/static/css/main.86390b65.chunk.css"
+  },
+  {
+    "revision": "f93a6bd60272f049ca484de01f11468b",
+    "url": "/static/media/my-cartoon.f93a6bd6.png"
+  },
+  {
+    "revision": "e5f6c8a9b96177c484f6",
+    "url": "/static/js/1.e5f6c8a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a4afa69b34c95bcb593432de717c2201",
+    "url": "/static/media/logo.a4afa69b.png"
+  },
+  {
+    "revision": "b08cfa374b7a3a48a7241dcf23139133",
+    "url": "/static/media/icon-logo.b08cfa37.png"
+  },
+  {
+    "revision": "ddb2254b64fbbb52e1eed68b4c2a5b5a",
+    "url": "/static/media/profile-bg.ddb2254b.png"
+  },
+  {
+    "revision": "d1636e53b42f0796fca9862952512e09",
+    "url": "/static/media/my-cartoon2.d1636e53.png"
+  },
+  {
+    "revision": "cb499467d9c192b71164a05db64a2459",
+    "url": "/static/media/umass.cb499467.png"
+  },
+  {
+    "revision": "2ba6a1d5eefa217cc0eb06afbe000aca",
+    "url": "/static/media/bhcc.2ba6a1d5.png"
+  },
+  {
+    "revision": "9bc0884b6ccfee46a32080db9e1b7a55",
+    "url": "/static/media/django.9bc0884b.png"
+  },
+  {
+    "revision": "0ccb8695ac1408dffac87b76c1100a85",
+    "url": "/static/media/python.0ccb8695.png"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "554c132f3a468ea748aa",
+    "url": "/static/js/main.554c132f.chunk.js"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "3b3e2131600285cc86d55b191c0f879a",
+    "url": "/static/media/Leenawarat_resume.3b3e2131.pdf"
+  },
+  {
+    "revision": "549e2e6e1ae679666b344dc0eb469b5d",
+    "url": "/static/media/Blog-logo.549e2e6e.png"
+  },
+  {
+    "revision": "9b97f9d05f1850d6ea7f85b1af7194b1",
+    "url": "/static/media/UI_Poll.9b97f9d0.png"
+  },
+  {
+    "revision": "1b6f680e53e3501d56d24f230cfa88d7",
+    "url": "/static/media/UI_debate.1b6f680e.png"
+  },
+  {
+    "revision": "d81dab358c6a58ef33d9c67ab57f5446",
+    "url": "/static/media/UI_discussion.d81dab35.png"
+  },
+  {
+    "revision": "2b35939e8cf654a6038811e85f23f915",
+    "url": "/static/media/UI_home.2b35939e.png"
+  },
+  {
+    "revision": "a56d1e1492ed8d2b37e2d0900dfeb0ef",
+    "url": "/static/media/UI_user.a56d1e14.png"
+  },
+  {
+    "revision": "fd3b0da434005112db146cb3af7d17a6",
+    "url": "/static/media/chick.fd3b0da4.jpg"
+  },
+  {
+    "revision": "96ef2d2ed3c05904dc56a7fea1dc9664",
+    "url": "/static/media/dabate.96ef2d2e.png"
+  },
+  {
+    "revision": "075a7c868deb30992f907fe7db5eefcd",
+    "url": "/static/media/data_model.075a7c86.png"
+  },
+  {
+    "revision": "8e2b133e295d95c9e6dfb9d4aa86d5c5",
+    "url": "/static/media/diagram.8e2b133e.png"
+  },
+  {
+    "revision": "b68ef00844f4bd0bbcd4c958a8cb36b9",
+    "url": "/static/media/discussion.b68ef008.png"
+  },
+  {
+    "revision": "642fa6a2e0600768e603392104852b93",
+    "url": "/static/media/login.642fa6a2.png"
+  },
+  {
+    "revision": "4cb84b9e578f239418d9749d806514a6",
+    "url": "/static/media/pastpolls.4cb84b9e.png"
+  },
+  {
+    "revision": "09df4c80eff44fd8799dc122d7c5a30e",
+    "url": "/static/media/poll.09df4c80.png"
+  },
+  {
+    "revision": "c940dd053a538b93f696abb5d7b90ad0",
+    "url": "/static/media/profile.c940dd05.png"
+  },
+  {
+    "revision": "66b302bde247accfc6f35e0b6e200d0e",
+    "url": "/static/media/profile2.66b302bd.png"
+  },
+  {
+    "revision": "05c7ec3086fa4b11344b789c38219ebe",
+    "url": "/static/media/search.05c7ec30.png"
+  },
+  {
+    "revision": "46c172c96aeaca3804e1726ab36ab8cf",
+    "url": "/static/media/sign_up.46c172c9.png"
+  },
+  {
+    "revision": "7497c3fdd402587a6bf5b79d048110f6",
+    "url": "/static/media/bhcc.7497c3fd.jpg"
+  },
+  {
+    "revision": "e6a3e30b054726d2e046ea27c0392dbd",
+    "url": "/static/media/logo.e6a3e30b.gif"
+  },
+  {
+    "revision": "e5f6c8a9b96177c484f6",
+    "url": "/static/css/1.ac3b0ffb.chunk.css"
+  },
+  {
+    "revision": "ac52836367a016008ac0da57f2793d99",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a0bf8027dd1506b94ca81178fc86330.js"
+  "/precache-manifest.f6a3fa6c9889e175648c6a075d5eb815.js"
 );
 
 workbox.clientsClaim();
