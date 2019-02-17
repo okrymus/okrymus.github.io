595dd09f73c3fca92935d79fc59aa21c24d20b73
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-02-17 00:03:54 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.555ac27a1e1c3ad88ee8089c96566322.js"
+  "/precache-manifest.a25c7fc20614db41b114c8a13b7fc4fd.js"
 );
 
 workbox.clientsClaim();
