1b2269a8e1353257eb72d2cbe08b5f246b0c14ed
Okrymus inc <OKRYMUS@Panupongs-MacBook-Pro.local>
2019-02-17 00:15:31 -0500

    Updates

--- precache-manifest.a7ee06a8a87c5beb9409cd62cc485bb4.js
+++ precache-manifest.a7ee06a8a87c5beb9409cd62cc485bb4.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/static/media/IMG_3861.3b5f566c.JPG"
   },
   {
-    "revision": "487538b3a736dde3e22c",
+    "revision": "21eb05eab4342417ee02",
     "url": "/static/css/main.8c70eabc.chunk.css"
   },
   {
@@ -124,14 +124,6 @@ self.__precacheManifest = [
     "url": "/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "faff92145777a3cbaf8e7367b4807987",
-    "url": "/static/media/icons.faff9214.woff"
-  },
-  {
-    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
-    "url": "/static/media/icons.b87b9ba5.ttf"
-  },
-  {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
     "url": "/static/media/icons.0ab54153.woff2"
   },
@@ -140,36 +132,44 @@ self.__precacheManifest = [
     "url": "/static/media/outline-icons.cd6c777f.woff2"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/static/media/outline-icons.701ae6ab.eot"
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
   },
   {
     "revision": "962a1bf31c081691065fe333d9fa8105",
     "url": "/static/media/icons.962a1bf3.svg"
   },
   {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
     "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
     "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/static/media/outline-icons.ad97afd3.ttf"
   },
   {
-    "revision": "a046592bac8f2fd96e994733faf3858c",
-    "url": "/static/media/brand-icons.a046592b.woff"
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
   },
   {
     "revision": "13db00b7a34fee4d819ab7f9838cc428",
     "url": "/static/media/brand-icons.13db00b7.eot"
   },
   {
-    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
-    "url": "/static/media/outline-icons.82f60bd0.svg"
-  },
-  {
     "revision": "e8c322de9658cbeb8a774b6624167c2c",
     "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -372,8 +372,8 @@ self.__precacheManifest = [
     "url": "/static/media/IMG_1401.72b519a7.JPG"
   },
   {
-    "revision": "487538b3a736dde3e22c",
-    "url": "/static/js/main.487538b3.chunk.js"
+    "revision": "21eb05eab4342417ee02",
+    "url": "/static/js/main.21eb05ea.chunk.js"
   },
   {
     "revision": "b6f3f1feb58255f42d6425f291d2721f",
@@ -740,7 +740,7 @@ self.__precacheManifest = [
     "url": "/static/css/1.b4e3bc2c.chunk.css"
   },
   {
-    "revision": "d9fe2de8c1f3e1fb0d58a0cb91d5b822",
+    "revision": "99633241a854522c28e56d8ce21bbd57",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a25c7fc20614db41b114c8a13b7fc4fd.js"
+  "/precache-manifest.a7ee06a8a87c5beb9409cd62cc485bb4.js"
 );
 
 workbox.clientsClaim();
