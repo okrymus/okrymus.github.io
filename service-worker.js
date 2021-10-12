485e1bfca80338c94ed7686c4f826f38ebc18c35
Okrymus inc <panupong_lee@me.com>
2021-10-12 01:26:32 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0aaf8137df44a476b7638338a2730663.js"
+  "/precache-manifest.7fcaf80753c7e6f7d4ae70d15a575acf.js"
 );
 
 workbox.clientsClaim();
